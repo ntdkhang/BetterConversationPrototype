1891ec06a46817390403fa53e8473567df0c46a1
ntdkhang <ntdkhang143@gmail.com>
2024-02-21 10:31:24 -0600

    chat bubble

--- BetterConversationPrototype/ChatView.swift
+++ BetterConversationPrototype/ChatView.swift
@@ -11,27 +11,81 @@ struct ChatView: View {
     var messages: [String] = []
     var body: some View {
         VStack {
-            ChatBubble(message: "Vàng bạc mày to nhờ", isSender: false)
-            ChatBubble(message: "Cho bố cái địa chỉ", isSender: true)
-            ChatBubble(message: "Địt mẹ mày nói ít thôi", isSender: true)
+            ChatBubbleFromThem(message: "Vàng bạc mày to nhờ")
+            ChatBubbleFromMe(message: "Cho bố cái địa chỉ")
+            ChatBubbleFromMe(message: "Địt mẹ mày nói ít thôi")
+            ChatBubbleFromThem(message: "Vãi lồn luôn đầu cắt moi")
         }
     }
 }
 
-struct ChatBubble: View {
+struct AvatarIconView: View {
+    var imageName: String
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+    }
+}
+
+struct ChatBubbleFromMe: View {
     var message: String
-    var isSender: Bool
-    var alignment: Alignment {
-        isSender ? .trailing : .leading
+    var color: Color {
+        .blue
     }
 
     var body: some View {
-        VStack {
-            HStack {
-                Text(message)
+        HStack {
+            HStack(alignment: .bottom) {
+                HStack {
+                    Text(message)
+                }
+                .padding(12)
+                .frame(alignment: .trailing)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(color)
+                }
+                .padding(.leading, 24)
+
+                VStack {
+                    AvatarIconView(imageName: "DauCatMoi")
+                        .frame(width: 25)
+                }
             }
-            .frame(maxWidth: .infinity, alignment: alignment)
         }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+}
+
+struct ChatBubbleFromThem: View {
+    var message: String
+    var color: Color {
+        .gray
+    }
+
+    var body: some View {
+        HStack {
+            HStack(alignment: .bottom) {
+                VStack {
+                    AvatarIconView(imageName: "VaiLonLuon")
+                        .frame(width: 25)
+                }
+
+                HStack {
+                    Text(message)
+                }
+                .padding(12)
+                .frame(alignment: .leading)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(color)
+                }
+                .padding(.trailing, 24)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
