ea4fa8a2ca9802099f90bf7b6decbf531db35d64
ntdkhang <ntdkhang143@gmail.com>
2024-02-24 15:50:44 -0600

    Kha Banh

--- BetterConversationPrototype/User.swift
+++ BetterConversationPrototype/User.swift
@@ -32,4 +32,6 @@ extension User {
     static var me: User = .init(id: UUID(), name: "Khang Nguyen", DOB: Date.now, imageName: "VaiLonLuon", interests: .init(arrayLiteral: .career, .romance))
 
     static var them: User = .init(id: UUID(), name: "Sua Buoi", DOB: Date.now, imageName: "DauCatMoi", interests: .init(arrayLiteral: .meditation, .romance))
+
+    static var KhaBanh: User = .init(id: UUID(), name: "Kha Banh", DOB: Date.now, imageName: "KhaBanh", interests: .init(arrayLiteral: .family, .friendship))
 }
