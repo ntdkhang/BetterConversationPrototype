9ed128c9cb3bf2103a15a48084847708a365441e
ntdkhang <ntdkhang143@gmail.com>
2024-03-06 06:44:56 -0600

    remove all library except auth

--- BetterConversationPrototype/BetterConversationPrototypeApp.swift
+++ BetterConversationPrototype/BetterConversationPrototypeApp.swift
@@ -5,8 +5,17 @@
 //  Created by Khang Nguyen on 2/3/24.
 //
 
-import SwiftData
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BetterConversationPrototypeApp: App {
