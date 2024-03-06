95146c75e9a269d7581d4d12efc23f7df3310b87
ntdkhang <ntdkhang143@gmail.com>
2024-03-05 21:18:00 -0600

    im sleepy

--- BetterConversationPrototype/BetterConversationPrototypeApp.swift
+++ BetterConversationPrototype/BetterConversationPrototypeApp.swift
@@ -5,29 +5,15 @@
 //  Created by Khang Nguyen on 2/3/24.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct BetterConversationPrototypeApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             MenuView()
                 .preferredColorScheme(.dark)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
